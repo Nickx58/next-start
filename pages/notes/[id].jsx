9564b174e7911d79c19136d31042732643035e66
Nickx58 <nikhilx58@gmail.com>
2021-04-10 14:04:56 +0530

    useRouter hook to fetch id

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useRouter } from 'next/router'
 
-const NotePage = () => <div>ID note Page</div>
+const NotePage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return(
+    <h1>Notes {id}</h1>
+  )
+}
 
 export default NotePage;
